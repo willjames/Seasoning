18f9ad7e0f4eda6af8b5912d77785266535690e2
will.james@7digital.com <github@willjames.co.uk>
2011-12-01 17:20:23 +0000

    US seasoning WIP commit

---
+++ Seasoning/UsSeasonUtilities.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace Seasoning
+{
+	public class UsSeasonUtilities
+	{
+		public static bool IsMartinLutherKingBirthday(DateTime dateToEvaluate)
+		{
+			if (dateToEvaluate == GetThirdMondayInJanuary(dateToEvaluate))
+				return true;
+
+			return false;
+		}
+
+		private static DateTime GetThirdMondayInJanuary(DateTime dateToEvaluate)
+		{
+			var listOfMondaysInJanuary = new List<int>();
+			int daysinMonth = DateTime.DaysInMonth(dateToEvaluate.Year, dateToEvaluate.Month);
+
+			var dateForIteration = new DateTime(dateToEvaluate.Year, dateToEvaluate.Month, 1);
+
+			for (int i = 1; i<=daysinMonth; i++)
+			{
+				if (dateForIteration.DayOfWeek == DayOfWeek.Monday)
+					listOfMondaysInJanuary.Add(i);
+
+				dateForIteration = dateForIteration.AddDays(1);
+			}
+
+			return new DateTime(dateToEvaluate.Year, dateToEvaluate.Month, listOfMondaysInJanuary[2]);
+		}
+	}
+}
---
+++ SeasoningTests/UsSeasonUtilitiesTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Seasoning;
+
+namespace SeasoningTests
+{
+	[TestFixture]
+	public class UsSeasonUtilitiesTests
+	{
+
+		/*
+		 *	http://en.wikipedia.org/wiki/Public_holidays_in_the_United_States
+		 *	Date													Official Name
+			January 1												New Year's Day
+			Third Monday in January									Birthday of Martin Luther King, Jr.
+			First January 20th following a Presidential election	Inauguration Day
+			Third Monday in February								Washington's Birthday
+			Last Monday in May										Memorial Day
+			July 4													Independence Day
+			First Monday in September								Labor Day
+			Second Monday in October								Columbus Day
+			November 11												Veterans Day
+			Fourth Thursday in November								Thanksgiving Day
+			December 25												Christmas
+		 */
+
+		[Test]
+		public void Should_return_true_for_IsMartinLutherKingBirthday()
+		{
+			var dateToEvaluate = new DateTime(2012, 01, 16); //known third Monday in January
+
+			Assert.That(UsSeasonUtilities.IsMartinLutherKingBirthday(dateToEvaluate), Is.True);
+		}
+	}
+}
